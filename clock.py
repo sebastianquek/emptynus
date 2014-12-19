13d9bd6c8357cf140837e2080885fd50ee1e2887
Sebastian <sebq13@gmail.com>
2014-12-19 22:53:47 +0800

    Fixed sizing of search parameters

--- clock.py
+++
@@ -1,9 +0,0 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
-
-sched = BlockingScheduler()
-
-@sched.scheduled_job('interval', hours=24)
-def timed_job():
-    print('This job is run every 24 hours.')
-
-sched.start()
