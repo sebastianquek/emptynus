0544a3c06870d2677ef6d1b8316edce38e6b4aac
Sebastian <sebq13@gmail.com>
2014-12-20 20:11:58 +0800

    Downgrade to python2

--- static/task.py
+++ static/task.py
@@ -1,4 +1,4 @@
-import urllib.request
+import urllib
 import os
 import heroku3
 
@@ -8,7 +8,7 @@ URL = "http://api.nusmods.com/{}/{}/timetable.json".format(ACAD_YEAR, SEM)
 
 print(os.path.dirname(os.path.realpath(__file__)))
 print("Downloading " + URL)
-print(urllib.request.urlretrieve(URL, "./static/timetable.json"))
+print(urllib.urlretrieve(URL, "./static/timetable.json"))
 print("Downloaded successfully.")
 
 print("Restarting dyno")
