a70cc8e5281999893949da1d51943eeaafe7eeed
Sebastian <sebq13@gmail.com>
2014-12-20 19:45:15 +0800

    Attempt to restart dyno after downloading timetable.json

--- static/task.py
+++ static/task.py
@@ -1,5 +1,6 @@
 import urllib.request
 import os
+import heroku3
 
 ACAD_YEAR = "2014-2015"
 SEM = "2"
@@ -8,4 +9,17 @@ URL = "http://api.nusmods.com/{}/{}/timetable.json".format(ACAD_YEAR, SEM)
 print(os.path.dirname(os.path.realpath(__file__)))
 print("Downloading " + URL)
 print(urllib.request.urlretrieve(URL, "./static/timetable.json"))
-print("Done.")
+print("Downloaded successfully.")
+
+print("Restarting dyno")
+HEROKU_TOKEN = "8f8bc684-8a7f-4ce0-ad92-1875a0c0c233"  #used for instance restart
+APP_NAME = "emptynus"
+def restart_heroku(auto=False):
+    '''restart heroku instance to re-allocate IP'''
+    cloud = heroku3.from_key(HEROKU_TOKEN)
+    app = cloud.apps[APP_NAME]
+    for p in app.processes:
+        p.restart()
+
+restart_heroku()
+print("Restarted successfully.")
