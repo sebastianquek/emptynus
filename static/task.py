6a6f9d030c6e0fb516394209ee0a02fe5f3b0267
Sebastian <sebq13@gmail.com>
2014-12-20 17:12:13 +0800

    Added code to see directory

--- static/task.py
+++ static/task.py
@@ -1,9 +1,11 @@
 import urllib.request
+import os
 
 ACAD_YEAR = "2014-2015"
 SEM = "2"
 URL = "http://api.nusmods.com/{}/{}/timetable.json".format(ACAD_YEAR, SEM)
 
+print(os.path.dirname(os.path.realpath(__file__)))
 print("Downloading " + URL)
 print(urllib.request.urlretrieve(URL, "./static/timetable.json"))
 print("Done.")
