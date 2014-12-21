4a89ddccfffa2974f822ad7c64d2bbc0a5ff623c
Sebastian <sebq13@gmail.com>
2014-12-21 11:37:55 +0800

    Implemented the use of environmental variables. Added js to gitignore

--- task.py
+++ task.py
@@ -1,19 +1,11 @@
-# import urllib
-# import os
+import os
 import heroku3
 
-# ACAD_YEAR = "2014-2015"
-# SEM = "2"
-# URL = "http://api.nusmods.com/{}/{}/timetable.json".format(ACAD_YEAR, SEM)
-
-# print(os.path.dirname(os.path.realpath(__file__)))
-# print("Downloading " + URL)
-# print(urllib.urlretrieve(URL, "timetable.json"))
-# print("Downloaded successfully.")
-
 print("Restarting dyno")
-HEROKU_TOKEN = "8f8bc684-8a7f-4ce0-ad92-1875a0c0c233"  #used for instance restart
+
+HEROKU_TOKEN = os.environ["HEROKU_TOKEN"]
 APP_NAME = "emptynus"
+
 def restart_heroku():
 	cloud = heroku3.from_key(HEROKU_TOKEN)
 	app = cloud.apps()[APP_NAME]
@@ -21,4 +13,5 @@ def restart_heroku():
 		dyno.kill()
 
 restart_heroku()
+
 print("Restarted successfully.")
